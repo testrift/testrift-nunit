704c8711092e511634d19ac97325ea6866fcdc52
Andreas Anderberg <andreas@orbital-systems.com>
2026-01-19 13:30:52 +0100

    Example: Enable parallel test execution

--- Example/ExampleTests.cs
+++ Example/ExampleTests.cs
@@ -7,6 +7,8 @@ using NUnit.Framework;
 using TestRift.NUnit;
 
 [assembly: TRLogger]
+[assembly: Parallelizable(ParallelScope.All)]
+[assembly: LevelOfParallelism(8)]   // choose based on CPU / IO
 
 namespace ExampleTests
 {
